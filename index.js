e796c4f7a89687a601a37f88486af4662d9a0927
thiagotp <thiagoteixeira.ps@gmail.com>
2021-02-22 21:35:48 -0300

    Alterando comportamento do submit, adicionando eventos e adicionando informações do usuário na tabela

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-var user = {}
-
 //Trazendo todos os campos do formulário que contem um atributo "name"
 var fields = document.querySelectorAll("#form-user-create [name]")
-//Percorrendo os campos encontrados
-fields.forEach((field, index)=>{
+var user = {}
 
-    if(field.name === "gender" && field.checked){
-        user[field.name] = field.value
-    }else{
-        user[field.name] = field.value
-    }
+//função que servirá para adicionar uma linha de exibição na lista de usuários
+//o innerHTML serve para adicionar um texto html e esse texto será tratado como comando
+function addLine(dataUser) {
+    var tr = document.createElement("tr")
+    tr.innerHTML = `
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    `
+    document.getElementById('table-users').appendChild(tr)
+    console.log(dataUser)
+}
 
+//Com o preventDefault eu cancelo a ação padrão do "submit" impedindo que ele atualize a página
+document.getElementById('form-user-create').addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //Percorrendo os campos encontrados
+    fields.forEach((field, index) => {
+
+        if (field.name === "gender" && field.checked) {
+            user[field.name] = field.value
+        }
+        if (field.name !== "gender") {
+            user[field.name] = field.value
+        }
+
+    })
+    addLine(user)
 })
-
-console.log(user)
